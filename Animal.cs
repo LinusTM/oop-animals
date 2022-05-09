3a666f7dcbd1dfe8ba42b903d3ed38d64c081b32
Emil <emil@Emils-MacBook-Pro.local>
2022-05-09 12:06:06 +0200

    Made getters, setters, and a constructor for Animal.

--- Animal.cs
+++ Animal.cs
@@ -2,9 +2,31 @@ namespace oop_animals;
 
 internal abstract class Animal {
     protected int legs;
+    internal int Legs {
+        get => legs;
+    }
+
     protected string foodType;
+    internal string FoodType {
+        get => foodType;
+    }
+
     protected double lifeSpan;
+    internal double LifeSpan {
+        get => lifeSpan;
+    }
+
     protected string legType;
+    internal string LegType {
+        get => legType;
+    }
+
+    Animal(int legs, string foodType, double lifeSpan, string legType) {
+        this.legs = legs;
+        this.foodType = foodType;
+        this.lifeSpan = lifeSpan;
+        this.legType = legType;
+    }
 
     internal abstract void Breathe();
     internal abstract void Delivery();
