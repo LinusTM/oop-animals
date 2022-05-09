add4de60d885347b836451b1d60af56931a4214d
ltd <9331149-LinusTD@users.noreply.gitlab.com>
2022-05-09 11:56:20 +0200

    mammal initial

--- Mammal.cs
+++ Mammal.cs
@@ -1,5 +1,13 @@
 namespace oop_animals;
 
 internal abstract class Mammal : Animal {
-    internal Mammal() { }
+    internal Mammal() : base() { }
+
+    override string Breathe() {
+        return "lungs";
+    }
+    
+    override string delivery() {
+        return "alive";
+    }
 }
